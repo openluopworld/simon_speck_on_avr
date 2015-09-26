0893aee4109725a623105ad35229e972a9994465
openluopworld <wuhanluop@163.com>
2015-09-25 18:21:07 -0700

    luopeng_

--- C++_3/simon-speck.h
+++ C++_3/simon-speck.h
@@ -53,8 +53,8 @@ inline u32 ror32 (u32 x, int n);
 inline u64 ror64 (u64 x, int n);
 
 /*
- * Simon£ºword size is 32
- * inputKey£ºthe original keys
+ * Simon: word size is 32
+ * inputKey: the original keys
  * keys: round keys
  */
 void setSimonKeys32 ( u32 * inputKey, u32 * keys );
@@ -67,33 +67,33 @@ void setSimonKeys32 ( u32 * inputKey, u32 * keys );
 void setSimonKeys64 ( u64 * inputKey, u64 * keys );
 
 /*
- * Simon Encryption£ºword size is 32
- * plainText£ºplainText[0] is the higher word, [1] is the lower. 
+ * Simon Encryption: word size is 32
+ * plainText: plainText[0] is the higher word, [1] is the lower. 
  *            plainText has just one block.
- * keys£ºround keys
+ * keys: round keys
  */
 void encryptionSimon32 ( u32 * plainText, u32 * keys );
 
 /*
- * Simon Decryption£ºword size is 32
- * cipherText£ºcipherText[0] is the higher word, [1] is the lower.
+ * Simon Decryption: word size is 32
+ * cipherText: cipherText[0] is the higher word, [1] is the lower.
  *             cipherText has just one block.
- * keys£ºround keys
+ * keys: round keys
  */
 void decryptionSimon32 ( u32 * cipherText, u32 * keys );
 
 /*
- * Simon Encryption£ºword size is 64
- * plainText£ºplainText[0] is the higher word, [1] is the lower. 
+ * Simon Encryption: word size is 64
+ * plainText: plainText[0] is the higher word, [1] is the lower. 
  *            plainText has just one block.
- * keys£ºround keys
+ * keys: round keys
  */
 void encryptionSimon64 ( u64 * plainText, u64 * keys );
 
 /*
- * Simon Decryption£ºword size is 64
- * cipherText£ºcipherText[0] is the higher word, [1] is the lower.
+ * Simon Decryption: word size is 64
+ * cipherText: cipherText[0] is the higher word, [1] is the lower.
  *             cipherText has just one block.
- * keys£ºround keys
+ * keys: round keys
  */
 void decryptionSimon64 ( u64 * cipherText, u64 * keys );
